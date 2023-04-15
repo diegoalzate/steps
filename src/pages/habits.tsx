43d88abdb8ac32891ecfe07ca22d54ecddf5916f
Diego Alzate <diego35.da@gmail.com>
2023-04-15 10:34:41 -0500

    boilerplate sections for v1

---
+++ src/pages/habits.tsx
@@ -0,0 +1,14 @@
+import { type NextPage } from "next";
+
+const Habits: NextPage = () => {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center">
+        <div>add new habit</div>
+        <div>show your habits</div>
+      </main>
+    </>
+  );
+};
+
+export default Habits;
