857bc3a255c76d501c4b9bb0b82af02db4867be1
Diego Alzate <diego35.da@gmail.com>
2023-04-16 18:39:05 -0500

    add is public paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,28 @@
-import { withClerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { getAuth, withClerkMiddleware } from "@clerk/nextjs/server";
+import { type NextRequest, NextResponse } from "next/server";
 
-export default withClerkMiddleware(() => {
+// Set the paths that don't require the user to be signed in
+const publicPaths = ["/"];
+
+const isPublic = (path: string) => {
+  return publicPaths.find((x) =>
+    path.match(new RegExp(`^${x}$`.replace("*$", "($|/)")))
+  );
+};
+
+export default withClerkMiddleware((request: NextRequest) => {
+  if (isPublic(request.nextUrl.pathname)) {
+    return NextResponse.next();
+  }
+  // if the user is not signed in redirect them to the sign in page.
+  const { userId } = getAuth(request);
+
+  if (!userId) {
+    // redirect the users to homepage
+
+    const homeUrl = new URL("/", request.url);
+    return NextResponse.redirect(homeUrl);
+  }
   return NextResponse.next();
 });
 
