2f9d626837f1249dc4b639d9ee92913e34846c20
Diego Alzate <diego35.da@gmail.com>
2023-04-16 13:16:36 -0500

    create HabitEntry schema and a bit more on habitlist ui

--- src/pages/habits.tsx
+++ src/pages/habits.tsx
@@ -1,5 +1,9 @@
 import { type NextPage } from "next";
-import { Frequency, SharingOptions } from "@prisma/client";
+import { Frequency, type Habit, SharingOptions } from "@prisma/client";
+import * as dayjs from "dayjs";
+
+const COMPLETED_HABIT = "🟠";
+const MISSING_HABIT = "⚪";
 
 const HabitForm = () => {
   return (
@@ -15,8 +19,8 @@ const HabitForm = () => {
           id="habit"
           name="habit"
           type="text"
-          placeholder="ex: I will meditate every day at 3pm at home"
-          className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
+          placeholder="ex: meditate every day at 3pm at home"
+          className="block w-full rounded-md border-0 bg-transparent px-2 py-1.5 text-gray-900 outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
         />
       </div>
       <div className="flex min-w-full items-baseline space-x-6">
@@ -28,7 +32,7 @@ const HabitForm = () => {
           name="amount"
           type="number"
           placeholder="3"
-          className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
+          className=" block w-full rounded-md border-0 bg-transparent px-2 py-1.5 text-gray-900 outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
         />
       </div>
       <div className="flex min-w-full items-baseline space-x-6">
@@ -38,7 +42,7 @@ const HabitForm = () => {
         <select
           name="frequencySelect"
           id="frequencySelect"
-          className="block rounded-md border-0 px-2 py-1.5 text-gray-900 outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
+          className="block rounded-md border-0 bg-transparent px-2 py-1.5 text-gray-900 outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
         >
           {Object.values(Frequency).map((frequency) => (
             <option value="frequency" key={frequency}>
@@ -74,16 +78,41 @@ const HabitForm = () => {
 };
 
 const HabitList = () => {
-  return <h1>your habits</h1>;
+  return (
+    <div className="flex min-w-full flex-col space-y-2">
+      <h1 className="text-5xl font-bold text-amber-600">your habits</h1>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+        {[1, 2, 3, 4, 5].map((_, key) => (
+          <HabitCard key={key} />
+        ))}
+      </div>
+    </div>
+  );
 };
+
+const HabitCard = () => {
+  // get habit entries
+  // if daily habit get entries from this week
+  // if weekly or monthly habit get entries from this month
+
+  return (
+    <div className="border-1 min-h-max flex-col space-y-2 shadow-sm">
+      <div>
+        <h4> gym +</h4>
+      </div>
+      <span>4/5</span>
+    </div>
+  );
+};
+
 const Habits: NextPage = () => {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-center">
+      <main className="flex min-h-screen flex-col items-center justify-center space-y-2">
         <div className="w-4/5">
           <HabitForm />
         </div>
-        <div>{<HabitList />}</div>
+        <div className="w-4/5">{<HabitList />}</div>
       </main>
     </>
   );
