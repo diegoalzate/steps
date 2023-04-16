62b746558f65193e9a8dd4c26fd0669582a2a9e2
Diego Alzate <diego35.da@gmail.com>
2023-04-16 10:05:05 -0500

    polish form to create habits

--- src/pages/habits.tsx
+++ src/pages/habits.tsx
@@ -1,76 +1,89 @@
 import { type NextPage } from "next";
+import { Frequency, SharingOptions } from "@prisma/client";
 
+const HabitForm = () => {
+  return (
+    <form className="flex flex-col items-start gap-4 p-4 shadow-sm">
+      <div>
+        <h2 className="text-base leading-7">Create a new Habit</h2>
+      </div>
+      <div className="flex min-w-full items-baseline space-x-6">
+        <label className="block min-w-max text-sm font-medium leading-6 text-gray-900">
+          Habit
+        </label>
+        <input
+          id="habit"
+          name="habit"
+          type="text"
+          placeholder="ex: I will meditate every day at 3pm at home"
+          className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
+        />
+      </div>
+      <div className="flex min-w-full items-baseline space-x-6">
+        <label className="block min-w-max text-sm font-medium leading-6 text-gray-900">
+          Amount
+        </label>
+        <input
+          id="amount"
+          name="amount"
+          type="number"
+          placeholder="3"
+          className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
+        />
+      </div>
+      <div className="flex min-w-full items-baseline space-x-6">
+        <label className="block  min-w-max text-sm font-medium leading-6 text-gray-900">
+          Frequency
+        </label>
+        <select
+          name="frequencySelect"
+          id="frequencySelect"
+          className="block rounded-md border-0 px-2 py-1.5 text-gray-900 outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
+        >
+          {Object.values(Frequency).map((frequency) => (
+            <option value="frequency" key={frequency}>
+              {frequency.toLocaleLowerCase()}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="flex min-w-full items-baseline space-x-6">
+        <label className="block min-w-max text-sm font-medium leading-6 text-gray-900">
+          Sharing options
+        </label>
+        <div className="flex flex-col gap-2">
+          {Object.values(SharingOptions).map((option) => (
+            <div key={option} className="flex space-x-2">
+              <input
+                type="radio"
+                name="sharingOption"
+                id={option}
+                value={option}
+                key={option}
+              />
+              <label htmlFor={option}>{option.toLocaleLowerCase()}</label>
+            </div>
+          ))}
+        </div>
+      </div>
+      <button className="max-w-md self-center rounded-lg border-2 border-amber-600 bg-amber-600 px-2 py-1 text-slate-200 hover:bg-slate-200 hover:text-black ">
+        save
+      </button>
+    </form>
+  );
+};
+
+const HabitList = () => {
+  return <h1>your habits</h1>;
+};
 const Habits: NextPage = () => {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-center">
-        <div className="min-w-full border-b border-gray-900/10 p-2">
-          <form className="flex flex-col items-start">
-            <div>
-              <h2 className="text-base leading-7">Create a new Habit</h2>
-            </div>
-            <div className="flex items-baseline space-x-6">
-              <label className="block text-sm font-medium leading-6 text-gray-900">
-                Habit
-              </label>
-              <div className="mt-2">
-                <input
-                  id="habit"
-                  name="habit"
-                  type="text"
-                  autoComplete="habit"
-                  className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div className="flex items-baseline space-x-6">
-              <label className="block text-sm font-medium leading-6 text-gray-900">
-                Amount
-              </label>
-              <div className="mt-2">
-                <input
-                  id="amount"
-                  name="amount"
-                  type="number"
-                  autoComplete="amount"
-                  className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div className="flex items-baseline space-x-6">
-              <label className="block text-sm font-medium leading-6 text-gray-900">
-                Time
-              </label>
-              <div className="mt-2">
-                <input
-                  id="Time"
-                  name="Time"
-                  type="string"
-                  autoComplete="Time"
-                  className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div className="flex items-baseline space-x-6">
-              <label className="block text-sm font-medium leading-6 text-gray-900">
-                Sharing options
-              </label>
-              <div className="mt-2">
-                <input
-                  id="Sharing"
-                  name="Sharing"
-                  type="string"
-                  autoComplete="Sharing"
-                  className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400  sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <button className="max-w-md rounded-lg border-2 border-amber-600 bg-amber-600 px-2 py-1 text-slate-200 hover:bg-slate-200 hover:text-black">
-              save
-            </button>
-          </form>
+        <div className="w-4/5">
+          <HabitForm />
         </div>
-        <div>show your habits</div>
+        <div>{<HabitList />}</div>
       </main>
     </>
   );
