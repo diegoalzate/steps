adba22f570cd42de95d8a821623db0ec6eb37790
Diego Alzate <diego35.da@gmail.com>
2023-05-25 21:54:56 -0500

    add leave group

--- src/pages/groups/[id].tsx
+++ src/pages/groups/[id].tsx
@@ -4,8 +4,9 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import { Button, HabitForm, HabitList } from "~/Components";
 import { api } from "~/utils/api";
-import { ShareIcon } from "@heroicons/react/24/outline";
+import { ShareIcon, TrashIcon } from "@heroicons/react/24/outline";
 import { toast } from "react-hot-toast";
+
 const Creator = ({ groupId }: { groupId: string }) => {
   const [habitIsOpen, setHabitIsOpen] = useState(false);
 
@@ -20,6 +21,7 @@ const Creator = ({ groupId }: { groupId: string }) => {
   }
 };
 
+
 const HabitPage: NextPage = () => {
   const router = useRouter();
   const ctx = api.useContext();
@@ -31,12 +33,20 @@ const HabitPage: NextPage = () => {
       void ctx.groupUsers.invalidate();
     },
   });
+  const { mutate: leaveGroup } = api.groupUsers.delete.useMutation({
+    onSuccess: () => {
+      toast.success("left group")
+      void ctx.groupUsers.invalidate();
+      router.back()
+    },
+  })
+
   const { isSignedIn } = useAuth();
 
   if (isLoading) return <span>loading...</span>;
   if (!group) return <span>oops looks like this groups does not exist</span>;
 
-  if (!groupUser)
+  if (!groupUser) {
     return (
       <main className="flex min-h-screen min-w-full flex-col items-center justify-center space-y-2 ">
         <div id="header" className="flex flex-col">
@@ -63,6 +73,7 @@ const HabitPage: NextPage = () => {
         )}
       </main>
     );
+  }
 
   return (
     <>
@@ -76,26 +87,30 @@ const HabitPage: NextPage = () => {
           </div>
         ) : null}
         <div id="header" className="flex w-4/5 flex-col">
-          <div className="flex items-baseline space-x-4">
+          <div className="flex items-baseline justify-between space-x-4">
             <h2 className="text-5xl font-bold text-amber-600">{group?.name}</h2>
-            <ShareIcon
-              className="h-6 w-6 hover:cursor-pointer"
-              onClick={() => {
-                navigator.clipboard
-                  .writeText(location.origin + router.asPath)
-                  .then(() => {
-                    toast.success("copied link to clipboard");
-                  })
-                  .catch(() => console.error("error copying to clipboard"));
-              }}
-            />
+            <div className="flex">
+              <ShareIcon
+                className="h-6 w-6 hover:cursor-pointer"
+                onClick={() => {
+                  navigator.clipboard
+                    .writeText(location.origin + router.asPath)
+                    .then(() => {
+                      toast.success("copied link to clipboard");
+                    })
+                    .catch(() => console.error("error copying to clipboard"));
+                }}
+              />
+              <TrashIcon className="h-6 w-6 hover:cursor-pointer" onClick={() => {
+                if (id) {
+                  leaveGroup(id)
+                }
+              }} />
+            </div>
           </div>
           <h3 className="text-base font-light text-amber-600">
             {group?.description}
           </h3>
-          {
-            // leave group
-          }
         </div>
         <div id="creator"></div>
         <div id="habitList" className="flex w-4/5 flex-col">
--- src/server/api/routers/groupUsers.ts
+++ src/server/api/routers/groupUsers.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, privateProcedure } from "~/server/api/trpc";
@@ -22,4 +23,38 @@ export const groupUsersRouter = createTRPCRouter({
       },
     });
   }),
+  delete: privateProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      const groupUser = await ctx.prisma.group_User.findFirst({
+        where: {
+          groupId: input,
+          userId: ctx.userId,
+        },
+      });
+
+      if (!groupUser) {
+        throw new TRPCError({ code: "UNAUTHORIZED" });
+      }
+
+      // leave group if user is not admin
+      if (groupUser?.role !== "ADMIN") {
+        await ctx.prisma.group_User.delete({
+          where: {
+            id: groupUser?.id,
+          },
+        });
+
+        return true;
+      }
+
+      // delete group if user is admin
+      await ctx.prisma.group.delete({
+        where: {
+          id: input,
+        },
+      });
+
+      return true;
+    }),
 });
