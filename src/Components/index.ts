5509cf9cdfb1c4662eeb86ec0ddd46523edba998
Diego Alzate <diego35.da@gmail.com>
2023-05-27 16:58:22 -0500

    remove old cached file

--- src/Components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as HabitCard } from "./HabitCard";
-export { default as HabitList } from "./HabitList";
-export { default as Button } from "./Button";
-export { default as HabitForm } from "./HabitForm";
-export { default as DropdownMenu } from "./DropdownMenu";
